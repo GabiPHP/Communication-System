323721e16af75df820db91af726fe2eced2cee68
GabiPHP <gabikebabgx@gmail.com>
2026-02-03 15:05:02 +0100

    Update URL variable for remote request

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ ID = {}
 stop_flag = False
 
 print("Checking for updates...")
-remote = requests.get(url,verify=False).text.replace("\r\n", "\n").strip()
+remote = requests.get(githubURL,verify=False).text.replace("\r\n", "\n").strip()
 with open(__file__,"r", encoding="utf-8-sig") as f:
     local = f.read().replace("\r\n", "\n").strip()
 if(remote != local):
